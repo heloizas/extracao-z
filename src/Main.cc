fa98837866a0847ad516405dd0d84b73c3a4d4ff
Heloiza Aparecida dos Santos <heloiza@MacBook-Pro-de-Heloiza.local>
2021-02-05 01:38:21 -0300

    Read file, create matrix

--- src/Main.cc
+++ src/Main.cc
@@ -1,7 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
 #include "Test.h"
 
+using namespace std;
+
 int main(int argc, char* argv[]) {
-    Test* pTest = new Test();
-    pTest->Print();
+    int linhas, colunas;
+    ifstream fileMap(argv[1]);
+
+    fileMap >> linhas >> colunas;
+
+    // Número de linhas e colunas da matriz
+    char MapMatrix[linhas][colunas];
+
+    // Adicionando elementos do mapa na matriz
+    int i, j;
+    for(i=0; i<linhas; i++) {
+		for(j=0; j<colunas; j++) { 
+            fileMap >> MapMatrix[i][j];
+		}
+	}
+
+    cout << MapMatrix[9][4];   
+
     return 0;
 }
