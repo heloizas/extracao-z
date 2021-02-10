6cb03602645e0569cf5953f3f5d6c5bccddf0f59
Heloiza Aparecida dos Santos <heloiza@MacBook-Pro-de-Heloiza.local>
2021-02-10 11:59:24 -0300

    debug

--- src/Main.cc
+++ src/Main.cc
@@ -1,18 +1,22 @@
 #include <iostream>
-#include <fstream>
+// #include <fstream>
 
-#include "../include/Mapa.h"
-#include "../include/Comandos.h"
-#include "../include/ListaRobos.h"
-#include "../include/Fila.h"
+// #include "../include/Mapa.h"
+// #include "../include/Comandos.h"
+// #include "../include/ListaRobos.h"
+// #include "../include/Fila.h"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
     // Primeiro arquivo é o mapa e o segundo os comandos
-    ifstream arquivoMapa(argv[1]);
-    ifstream arquivoComandos(argv[2]);
+    // ifstream arquivoMapa(argv[1]);
+    // ifstream arquivoComandos(argv[2]);
 
+    string a;
+    string b;
+    a = "Oi";
+    cout << a;
     // mapa.imprimirFila();
     // mapa.imprimirMapa();
 
